7f05c7e86203b41beeba799b227dff0449004ba6
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2019-06-24 16:45:59 -0300

    exibe os erros por javascript

--- src/admin/assets/js/admin.js
+++ src/admin/assets/js/admin.js
@@ -1,6 +1,14 @@
 (function($){
     $(function(){
         $('.js-mapas--import-new-entities').on('click', API.importNewEntities);
+
+        if(mc_errors){
+            for(var prop in mc_errors){
+                $('input#' + prop).css({border: '2px solid red'});
+                var errors = mc_errors[prop].concat(', ');
+                $('input#' + prop).after("<br><em>" + errors + '</em>');
+            }
+        }
     });
 
     var API = {
