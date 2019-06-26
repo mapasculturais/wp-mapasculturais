aeb9c87300c22b684af57b5545350643f9cecfe8
Leonardo <leopiccionia@gmail.com>
2019-06-26 17:52:44 -0300

    List calendar: initial commit.

---
+++ src/views/list.php
@@ -0,0 +1,90 @@
+<div class="mc-w-list">
+    <div class="mc-w-list--header">
+        <button class="previous"><</button>
+        <strong>Maio 2019</strong>
+        <button class="next">></button>
+    </div>
+    <div class="mc-w-list--content">
+        <div class="place mc-w-list--day">
+            <div class="mc-w-list--date">
+                <div class="day">3</div>
+                <div class="weekday">Quinta</div>
+            </div>
+            <div class="mc-w-list--events">
+                <div class="mc-w-list--event">
+                    <div class="category"><i>Artes circense</i> | Livre</div>
+                    <div class="description">
+                        <div class="name">Nome de um primeiro evento com nome grande</div>
+                        <div class="place">Condomínio Cultural</div>
+                    </div>
+                </div>
+                <div class="mc-w-list--event">
+                    <div class="category"><i>Música erudita</i> | 12 anos</div>
+                    <div class="description">
+                        <div class="name">Nome de um segundo evento</div>
+                        <div class="place">Museu das Bandeiras</div>
+                    </div>
+                </div>
+                <div class="mc-w-list--event">
+                    <div class="category"><i>Cinema</i> | 12 anos</div>
+                    <div class="description">
+                        <div class="name">Outro nome de um segundo evento</div>
+                        <div class="place">Biblioteca Monteiro Lobato</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="place mc-w-list--day">
+            <div class="mc-w-list--date">
+                <div class="day">4</div>
+                <div class="weekday">Sexta</div>
+            </div>
+            <div class="mc-w-list--events">
+                <div class="mc-w-list--event">
+                    <div class="category"><i>Artes visuais</i> | Livre</div>
+                    <div class="description">
+                        <div class="name">Nome de um primeiro evento</div>
+                        <div class="place">Biblioteca Itinerante de Quadrinhos</div>
+                    </div>
+                </div>
+                <div class="mc-w-list--event">
+                    <div class="category"><i>Arte indígena</i> | Livre</div>
+                    <div class="description">
+                        <div class="name">Nome de um primeiro evento com outro nome</div>
+                        <div class="place">Grupo Livre Capoeira Regional</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="place mc-w-list--day">
+            <div class="mc-w-list--date">
+                <div class="day">7</div>
+                <div class="weekday">Segunda</div>
+            </div>
+            <div class="mc-w-list--events">
+                <div class="mc-w-list--event">
+                    <div class="category"><i>Cinema</i> | 18 anos</div>
+                    <div class="description">
+                        <div class="name">Nome de um primeiro evento</div>
+                        <div class="place">Nome do espaço</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="place mc-w-list--day">
+            <div class="mc-w-list--date">
+                <div class="day">12</div>
+                <div class="weekday">Sábado</div>
+            </div>
+            <div class="mc-w-list--events">
+                <div class="mc-w-list--event">
+                    <div class="category"><i>Hip hop</i> | Livre</div>
+                    <div class="description">
+                        <div class="name">Nome de um primeiro evento</div>
+                        <div class="place">Livraria Dom Bosco</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
