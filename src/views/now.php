3c3d80ec417184f6df1a7aa33d48d367b3ad34cc
Leonardo <leopiccionia@gmail.com>
2019-06-27 17:38:11 -0300

    Events happening now.

---
+++ src/views/now.php
@@ -0,0 +1,67 @@
+<section class="mc-w mc-w-now">
+    <div class="mc-w--header mc-w-now--header">
+        <strong>Agora</strong>
+    </div>
+    <div class="mc-w-now--content">
+        <div class="mc-w-now--events">
+            <div class="mc-w--event mc-w-now--event">
+                <div class="time">12:00 - 23:30</div>
+                <div class="category"><i>Artes circenses</i> | Livre</div>
+                <div class="description">
+                    <i class="fas fa-check"></i>
+                    <div class="name">Nome de um primeiro evento com nome grande</div>
+                    <div class="place">Nome do espaço</div>
+                </div>
+            </div>
+            <div class="mc-w--event mc-w-now--event">
+                <div class="time">14:00 - 16:30</div>
+                <div class="category"><i>Teatro</i> | 12 anos</div>
+                <div class="description">
+                    <div class="name">Nome de um segundo evento</div>
+                    <div class="place">Nome do espaço</div>
+                </div>
+            </div>
+            <div class="mc-w--event mc-w-now--event">
+                <div class="time">14:30 - 18:30</div>
+                <div class="category"><i>Show</i> | 12 anos</div>
+                <div class="description">
+                    <div class="name">Outro nome de um segundo evento</div>
+                    <div class="place">Nome do espaço</div>
+                </div>
+            </div>
+            <div class="mc-w--event mc-w-now--event">
+                <div class="time">15:00 - 16:30</div>
+                <div class="category"><i>Cinema</i> | Livre</div>
+                <div class="description">
+                    <i class="fas fa-star"></i>
+                    <div class="name">Nome de um primeiro evento</div>
+                    <div class="place">Nome do espaço</div>
+                </div>
+            </div>
+            <div class="mc-w--event mc-w-now--event">
+                <div class="time">15:30 - 18:30</div>
+                <div class="category"><i>Show</i> | Livre</div>
+                <div class="description">
+                    <div class="name">Nome de um primeiro evento com outro nome</div>
+                    <div class="place">Nome do espaço</div>
+                </div>
+            </div>
+            <div class="mc-w--event mc-w-now--event">
+                <div class="time">15:30 - 18:30</div>
+                <div class="category"><i>Teatro</i> | 18 anos</div>
+                <div class="description">
+                    <div class="name">Nome de um primeiro evento</div>
+                    <div class="place">Nome do espaço</div>
+                </div>
+            </div>
+            <div class="mc-w--event mc-w-now--event">
+                <div class="time">15:40 - 18:30</div>
+                <div class="category"><i>Ao ar livre</i> | Livre</div>
+                <div class="description">
+                    <div class="name">Nome de um primeiro evento</div>
+                    <div class="place">Nome do espaço</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
