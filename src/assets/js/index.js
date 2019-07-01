da7607365d399d3298cd6c8f24917329745b6bf1
Leonardo <leopiccionia@gmail.com>
2019-07-01 18:00:33 -0300

    Refactor: no semis; don't pollute global namespace.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,17 +1,17 @@
-import Vue from 'vue';
+import Vue from 'vue'
 
-import WidgetAgenda from './components/MyAgenda.vue';
-import WidgetCalendar from './components/EventsCalendar.vue';
-import WidgetDay from './components/DailyEvents.vue';
-import WidgetList from './components/EventsList.vue';
-import WidgetNow from './components/HappeningNow.vue';
+import WidgetAgenda from './components/MyAgenda.vue'
+import WidgetCalendar from './components/EventsCalendar.vue'
+import WidgetDay from './components/DailyEvents.vue'
+import WidgetList from './components/EventsList.vue'
+import WidgetNow from './components/HappeningNow.vue'
 
-Vue.component('mc-w-agenda', WidgetAgenda);
-Vue.component('mc-w-calendar', WidgetCalendar);
-Vue.component('mc-w-day', WidgetDay);
-Vue.component('mc-w-list', WidgetList);
-Vue.component('mc-w-now', WidgetNow);
+Vue.component('mc-w-agenda', WidgetAgenda)
+Vue.component('mc-w-calendar', WidgetCalendar)
+Vue.component('mc-w-day', WidgetDay)
+Vue.component('mc-w-list', WidgetList)
+Vue.component('mc-w-now', WidgetNow)
 
-window.app = new Vue({
+new Vue({
     el: '#content'
-});
+})
