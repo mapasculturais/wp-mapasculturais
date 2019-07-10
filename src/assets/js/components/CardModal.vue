dbf4812543a1df3d877a9b02d18753e157a0f2f7
Leonardo <leopiccionia@gmail.com>
2019-07-10 15:57:25 -0300

    Card modals: complement.

--- src/assets/js/components/CardModal.vue
+++ src/assets/js/components/CardModal.vue
@@ -6,7 +6,7 @@
                     <a role="button" tabindex="0" @click="$emit('close')"><i class="fas fa-times"></i></a>
                 </div>
             </footer>
-            <header class="mc-w__card-header">
+            <header class="mc-w__card-header" :style="{ 'background-image': background }">
                 <div class="mc-w__card-slot">
                     <div class="icon"><i class="fas fa-circle"></i></div>
                     <div class="text">
@@ -27,6 +27,7 @@
     export default {
         name: 'CardModal',
         props: {
+            background: { type: String, default: '' },
             name: { type: String, required: true },
             category: { type: String, required: true },
             link: { type: String, required: true }
