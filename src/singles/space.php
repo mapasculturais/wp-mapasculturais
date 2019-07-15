5ae1ef87279c928aca9a5596eccd6f35db55847f
Leonardo <leopiccionia@gmail.com>
2019-07-15 16:14:57 -0300

    Space single: initial commit.

--- src/singles/space.php
+++ src/singles/space.php
@@ -1,9 +1,60 @@
-<?php 
-get_header('space'); 
+<?php
+get_header('space');
 the_post();
 $meta = get_post_meta(get_the_ID());
 ?>
 <h1>Espaço: <?php the_title() ?></h1>
 <article><?php the_content() ?></article>
-<?php var_dump($meta) ?> 
+<p><?php var_dump($meta) ?></p>
+
+<div class="mc-s mc-s-space">
+    <main class="mc-s__main">
+        <div class="mc-s__header" style="background-image: url(<?= wp_get_attachment_image_src($meta['space_header-image_thumbnail_id'][0], 'large')[0] ?>)"></div>
+        <div class="mc-s__title">
+            <div>
+                <div class="avatar" style="background-image: url(<?= wp_get_attachment_image_src($meta['MAPAS:entity_avatar_attachment_id'][0], 'thumbnail')[0] ?>)"></div>
+            </div>
+            <div>
+                <div class="type">Tipo do espaço</div>
+                <div class="title">Nome do espaço</div>
+                <div class="subtitle">Subtítulo do espaço</div>
+            </div>
+        </div>
+        <div class="mc-s__content">
+            <?php if (!empty($meta['shortDescription'])): ?>
+                <div class="mc-s__slot">
+                    <div class="icon"><span>Descrição</span></div>
+                    <div class="text"><?= $meta['shortDescription'][0] ?></div>
+                </div>
+            <?php endif; ?>
+            <div class="mc-s__slot">
+                <div class="icon"></div>
+                <div class="text"></div>
+            </div>
+            <div class="mc-s__slot">
+                <div class="icon"></div>
+                <div class="text"></div>
+            </div>
+            <?php if (!empty($meta['acessibilidade'])): ?>
+                <div class="mc-s__slot">
+                    <div class="icon" aria-label="Acessibilidade"><i class="fab fa-accessible-icon" aria-hidden="true"></i></div>
+                    <div class="text"><?= $meta['acessibilidade'][0] == 'Sim' ? 'Acessível' : 'Não acessível' ?></div>
+                </div>
+            <?php endif; ?>
+            <div class="mc-s__slot">
+                <div class="icon"></div>
+                <div class="text"></div>
+            </div>
+            <div class="mc-s__slot">
+                <div class="icon"></div>
+                <div class="text"></div>
+            </div>
+        </div>
+    </main>
+
+    <aside class="mc-s__sidebar">
+        Sidebar
+    </aside>
+</div>
+
 <?php get_footer('space'); ?>
